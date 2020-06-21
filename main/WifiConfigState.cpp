e496237fe64e71582f8cc7c9aae1dc6d1a6266a2
Byung Ju Hwang <pjhwang@gmail.com>
2020-06-21 18:36:14 +0900

    Implement WifiConfig

--- main/WifiConfigState.cpp
+++ main/WifiConfigState.cpp
@@ -11,9 +11,8 @@ static const char *TAG = "wifi_state";
 
 extern "C" {
 
-    const int capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(15);
+    const int capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(40);
     static StaticJsonDocument<capacity> scanlist;
-    static char json_wifilist[2048];
 
     static void wifi_setting_task(void *arg)
     {
@@ -35,41 +34,52 @@ extern "C" {
         WifiConfigState *s = (WifiConfigState *) c->getState();
 
         while (1) {
-            s->scan_nbr = WiFi.scanNetworks();
-            ESP_LOGI(TAG, "scan networks nbr: %d", s->scan_nbr);
+            if (xSemaphoreTake(s->_mutex, 10) == pdTRUE) {
+                s->scan_nbr = WiFi.scanNetworks();
+                ESP_LOGI(TAG, "scan networks nbr: %d", s->scan_nbr);
 
-            if (s->scan_nbr) {
+                if (s->scan_nbr) {
+                    if (scanlist.size()) {
+                        scanlist.clear();
+                    }
 
-                JsonArray list = scanlist.createNestedArray("wifi_list");
+                    JsonArray list = scanlist.createNestedArray("wifi_list");
 
-                if (s->scan_nbr > 15) {
-                    s->scan_nbr = 15;
+                    if (s->scan_nbr > 10) {
+                        s->scan_nbr = 10;
+                    }
+
+                    for (int i = 0; i < s->scan_nbr; i++) {
+                        JsonObject item = list.createNestedObject();
+                        item["ssid"] = WiFi.SSID(i);
+                        item["is_protected"] = (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? false : true;
+
+                        // ESP_LOGI(TAG, "\t%d: %s (%d) %c",
+                        //          i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i),
+                        //          (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? ' ' : '*');
+                    }
+
+                    // String wifilist;
+                    // serializeJson(scanlist, wifilist);
+                    // ESP_LOGI(TAG, "%s", wifilist.c_str());
                 }
-
-                for (int i = 0; i < s->scan_nbr; i++) {
-                    JsonObject item = list.createNestedObject();
-                    item["ssid"] = WiFi.SSID(i).c_str();
-                    item["is_protected"] = (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? false : true;
-
-                    ESP_LOGI(TAG, "\t%d: %s (%d) %c",
-                             i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i),
-                             (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? ' ' : '*');
-                }
+                xSemaphoreGive(s->_mutex);
             }
-            vTaskDelay(pdMS_TO_TICKS(500));
-        }
 
+            vTaskDelay(pdMS_TO_TICKS(3000));
+        }
     }
 
     static void wifi_server_task(void *arg)
     {
         Context *c = (Context *) arg;
         WifiConfigState *s = (WifiConfigState *) c->getState();
+        String header = "";
 
-        WiFi.softAP(s->ssid);
+        WiFi.softAP(s->ap_ssid.c_str());
         s->ip = WiFi.softAPIP();
-        //Serial.print("AP IP address: ");
-        //Serial.println(s->ip);
+        Serial.print("AP IP address: ");
+        Serial.println(s->ip);
 
         s->server.begin();
 
@@ -77,97 +87,94 @@ extern "C" {
 
             WiFiClient client = s->server.available();
             if (!client) {
-                vTaskDelay(pdMS_TO_TICKS(100));
+                delay(1);
                 continue;
             }
 
             ESP_LOGI(TAG, "New Client.");
-            String header;
+
             String currentLine = "";
             while (client.connected()) {
+
                 if (client.available()) {
+                    // String line = client.readStringUntil('\r');
+                    // printf("%s", line.c_str());
                     char c = client.read();
-                    printf("%c", c);
+                    putchar(c);
                     header += c;
-                    if (c == '\n') { // if the byte is a newline character
-                        // if the current line is blank, you got two newline characters in a row.
-                        // that's the end of the client HTTP request, so send a response:
 
-                        if (header.indexOf("GET /api/routers") >= 0) {
-                            if (currentLine.length() == 0) {
-                                int scan_nbr = WiFi.scanNetworks();
-                                ESP_LOGI(TAG, "scan networks nbr: %d", scan_nbr);
+                    if (c == '\n') {
+                        if (currentLine.length() == 0) {
+                            if (header.indexOf("GET /api/routers") >= 0) {
+                                if (xSemaphoreTake(s->_mutex, 100) == pdTRUE && scanlist.size() > 0) {
+                                    String wifilist;
+                                    serializeJson(scanlist, wifilist);
+                                    ESP_LOGI(TAG, "%s", wifilist.c_str());
 
-                                if (scan_nbr) {
-                                    JsonObject root = scanlist.to<JsonObject>();
-                                    JsonArray list = root.createNestedArray("wifi_list");
-
-                                    if (scan_nbr > 15) {
-                                        scan_nbr = 15;
-                                    }
-
-                                    json_wifilist[0] = '\0';
-                                    strcpy(json_wifilist, "{\"wifi_list\": [");
-
-                                    for (int i = 0; i < scan_nbr; i++) {
-                                        char json_item[256] = { 0 };
-                                        json_item[0] = '{';
-                                        sprintf(json_item, "{\"ssid\": \"%s\", \"is_protected\": %s},",
-                                                WiFi.SSID(i).c_str(),
-                                                (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? "false" : "true");
-                                        strcat(json_wifilist, json_item);
-
-                                        // JsonObject item = list.createNestedObject();
-                                        // item["ssid"] = WiFi.SSID(i);
-                                        // item["is_protected"] = (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? false : true;
-
-                                        // ESP_LOGI(TAG, "\t%d: %s (%d) %c",
-                                        //          i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i),
-                                        //          (WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?' ': '*');
-                                    }
-                                    json_wifilist[strlen(json_wifilist) - 1] = '\0';
-                                    strcat(json_wifilist, "]}\r\n\r\n");
-                                    printf("%s", json_wifilist);
-                                    // String output;
-                                    // serializeJsonPretty(scanlist, output);
-                                    // printf("%s\r\n", output.c_str());
-                                }
-
-                                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                                // and a content-type so the client knows what's coming, then a blank line:
-                                if (scan_nbr) {
                                     client.println(F("HTTP/1.1 200 OK"));
                                     client.println(F("Content-type: application/json"));
                                     client.println(F("Connection: close"));
                                     client.print(F("Content-Length: "));
-                                    client.println(strlen(json_wifilist));
-                                    // client.println(measureJson(scanlist));
+                                    client.println(measureJson(scanlist));
                                     client.println();
-                                    client.println(json_wifilist);
-                                    // serializeJson(scanlist, client);
-                                    //client.println();
+                                    serializeJson(scanlist, client);
+
+                                    xSemaphoreGive(s->_mutex);
                                 } else {
                                     client.println(F("HTTP/1.1 400 Bad Request"));
                                     client.println(F("Connection: close"));
                                     client.println();
                                 }
+                                break;
+
+                            } else if (header.indexOf("POST /api/routers/") >= 0) {
+                                DynamicJsonDocument resp(1024);
+                                resp["device_id"] = "1";
+
+                                vTaskSuspend(s->_scan_task_handle);
+
+                                String wifi_info = client.readString();
+                                String ssid = wifi_info.substring(wifi_info.indexOf("ssid="));
+                                String password = wifi_info.substring(wifi_info.indexOf("password="), wifi_info.indexOf("&"));
+                                ssid = ssid.substring(ssid.indexOf("=") + 1);
+                                password = password.substring(password.indexOf("=") + 1);
+                                printf("ssid = %s, password = %s\r\n", ssid.c_str(), password.c_str());
+
+                                client.println(F("HTTP/1.1 200 OK"));
+                                client.println(F("Content-type: application/json"));
+                                client.println(F("Connection: close"));
+                                client.print(F("Content-Length: "));
+                                client.println(measureJson(resp));
+                                client.println();
+                                serializeJson(resp, client);
+
+                                String resp_json;
+                                serializeJson(resp, resp_json);
+                                printf("%s", resp_json.c_str());
+                                vTaskResume(s->_scan_task_handle);
+                                break;
                             } else {
-                                currentLine = "";
+                                client.println(F("HTTP/1.1 400 Bad Request"));
+                                client.println(F("Connection: close"));
+                                client.println();
+                                break;
                             }
-                        } else if (c != '\r') {
-                            currentLine += c;
+
+                        } else {
+                            currentLine = "";
                         }
+                    } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                        currentLine += c;      // add it to the end of the currentLine
                     }
-                } else if (header.indexOf("POST /api/routers") >= 0) {
-                    // s->setInfo("test", "11112222");
                 }
-            }
+            } // end of client connected
+            vTaskDelay(pdMS_TO_TICKS(100));
+            // delay(1);
             header = "";
             client.stop();
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
+            Serial.println("[Client disconnected]");
+        } // end of while loop
+    } // the end of function
 }
 
 void WifiConfigState::buttonPressedLong(Context *c)
@@ -200,14 +207,14 @@ void WifiConfigState::begin(Context *c)
     c->printStringToLED("WIFI");
     c->changeButtonColor(LED::BLACK);
 
+    vSemaphoreCreateBinary(_mutex);
+
     xTaskCreatePinnedToCore(wifi_setting_task, "wifi_setting",
                             1024, c, 3, &_setting_task_handle, 0);
-
     xTaskCreatePinnedToCore(wifi_server_task, "wifi_server",
                             4096, c, 3, &_server_task_handle, 0);
-
-    //    xTaskCreatePinnedToCore(wifi_scan_task, "wifi_scan",
-    //                            4096, c, 3, &_scan_task_handle, 0);
+    xTaskCreatePinnedToCore(wifi_scan_task, "wifi_scan",
+                            2048, c, 3, &_scan_task_handle, 0);
 }
 
 void WifiConfigState::end(Context *c)
@@ -216,9 +223,11 @@ void WifiConfigState::end(Context *c)
     c->changeButtonColor(LED::BLACK);
 
     server.close();
-    //    vTaskDelete(_scan_task_handle);
+    vTaskDelete(_scan_task_handle);
     vTaskDelete(_server_task_handle);
     vTaskDelete(_setting_task_handle);
+
+    vSemaphoreDelete(_mutex);
 }
 
 void WifiConfigState::setInfo(const char *ssid, const char *password)
--- main/WifiConfigState.h
+++ main/WifiConfigState.h
@@ -8,37 +8,40 @@
 
 #include <WiFi.h>
 
-class WifiConfigState: public State {
+class WifiConfigState: public State
+{
 
 public:
-    static WifiConfigState* getInstance() {
+    static WifiConfigState *getInstance()
+    {
         static WifiConfigState instance;
         return &instance;
     }
 
     void buttonPressedLong(Context *c);
     void pressing10Sec(Context *c);
-    void setInfo(const char *ssid, const char* password);
+    void setInfo(const char *ssid, const char *password);
 
     WiFiServer server;
     IPAddress ip;
 
     // WiFiClient client;
     // String header;
-    const char * ssid = "PETTERN_KOTI";
-    //const char * password = "12345678";
+    const String ap_ssid = "PETTERN_KOTI";
 
     int scan_nbr;
-
-private:
-    WifiConfigState() {}
+    SemaphoreHandle_t _mutex = NULL;
 
     TaskHandle_t _setting_task_handle;
     TaskHandle_t _server_task_handle;
     TaskHandle_t _scan_task_handle;
 
+private:
+    WifiConfigState() {}
+
     void begin(Context *);
     void end(Context *);
+
 };
 
 #endif /* WIFICONFIGSTATE_H */
